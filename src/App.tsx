245729f72a59558d4bc94ebbacd7f6eb14946b08
ViralVault1 <robs95gt@hotmail.co.uk>
2025-09-08 19:37:49 +0100

    Minimal working App to fix build

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-gray-800 text-white min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Viral Video Factory</h1>
+        <p className="text-gray-400">Application is loading...</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
