095beea3a01fb3c670f4884116cd22f4afd55efc
ViralVault1 <robs95gt@hotmail.co.uk>
2025-09-18 00:57:08 +0100

    Add files via upload

---
+++ src/GifGeneratorPage.tsx.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function GifGeneratorPage({ onNavigate, initialFile, onFileConsumed }) {
+  return (
+    <div>
+      <h2>Gif Generator Page</h2>
+      {/* TODO: Add your gif generator logic or UI elements here */}
+      <p>This is a placeholder for the Gif Generator Page component.</p>
+    </div>
+  );
+}
