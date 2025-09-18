03611d173df1f260287e6fdca3b7c566bea8809a
ViralVault1 <robs95gt@hotmail.co.uk>
2025-09-18 10:31:08 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-for **** sake where is it now import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import './App.css';
