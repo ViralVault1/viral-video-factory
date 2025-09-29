fa7fac5619067a317a50cf3fd3f597cfbcbc2e4c
ViralVault1 <robs95gt@hotmail.co.uk>
2025-09-29 12:09:18 +0100

    Update openai-generate.js

--- api/openai-generate.js
+++ api/openai-generate.js
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
-export default async function handler(req, res) {
+export default async function handler(req: any, res: any) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
