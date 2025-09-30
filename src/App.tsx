45a383bcf65a2ac5062eae5799eb7d75206747a0
ViralVault1 <robs95gt@hotmail.co.uk>
2025-09-30 01:56:42 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { LicenseGeneratorPage } from './pages/LicenseGeneratorPage';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
